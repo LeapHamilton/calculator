79d984a36c59ae94fd6b397a2f25fdca7f817b84
Richard D <richard_dowling@hotmail.com>
2022-06-15 20:29:32 +0200

    basic function

---
+++ java.js
@@ -0,0 +1,19 @@
+function addNumbers(a,b) {
+    return a + b
+}
+console.log(addNumbers(10,100));
+
+function subtractNumbers(a,b) {
+    return a - b
+}
+console.log(subtractNumbers (10, 2));
+
+function multiplyNumbers(a,b){
+    return a * b 
+}
+console.log(multiplyNumbers(1000,2000));
+
+function divideNumbers(a,b) {
+    return a / b
+}
+console.log(divideNumbers(520,2));

edd9e47d2280a1d93f95e079f8f58d27fd2df5b5
Richard D <richard_dowling@hotmail.com>
2022-06-20 21:24:03 +0200

    got all clear working

--- java.js
+++ java.js
@@ -1,35 +1,79 @@
 const display = document.querySelector(".calculator-screen");
 
-let decimalPoint = false
-let currentInput = []
-let mathOperator
-let totalOperation
-
+let decimalPoint = false;
+let isAfterOperator = false;
+let whichOperator 
+let firstInput = [];
+let mathOperator;
+let secondInput = [];
 let displayValue = display.value;
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        
-      checkInput(button.id) /* the button.id will be called inputNumber later*/
+   
+      checkInput(button.id) /* button.id referenced as inputNumber or operator in later function parameters*/
     })})
 
 
 function checkInput(inputNumber) {
+
+    if (inputNumber === "equal") {
+       
+        equalStuff();
+    }
+  
+    if (inputNumber === "+" || inputNumber === "-" || inputNumber === "/" || inputNumber === "*") {
+        useOperator(inputNumber);
+    }
+
     if (!isNaN(inputNumber)) {
+      
         displayAndAddToUserInput(inputNumber);
 }
-    if (inputNumber === "." && decimalPoint === false) {
-        displayAndAddToUserInput(inputNumber);
+    if (inputNumber === "point" && decimalPoint === false) {
+        displayAndAddToUserInput(".");
         decimalPoint = true
     }
+
+    if (inputNumber === "Del") {
+        inputNumberPop = deleteNumber();
+
+    }
+    if (inputNumber ==="AC") {
+        console.log("Bippity Bop")
+        clearAllNumbers = clearAll();
+    }
+
 }
+
+function equalStuff (){
+    console.log("Toppity Tip!")
+    secondInput = display.value;
+    if (whichOperator === "+") {
+        display.value = addNumbers(firstInput, secondInput) * 1;
+}
+
+
+}
+
+function useOperator(operator) {
+ console.log(operator)
+    whichOperator = operator;
+    firstInput = display.value;
+    isAfterOperator = true;
+    decimalPoint = false;
+} 
 
 
 function displayAndAddToUserInput(inputNumber) {
-    console.log(inputNumber);
+    /*console.log(inputNumber);*/
+    if (isAfterOperator === true) {
+        display.value="";
+        isAfterOperator = false;
+    }
     displayNumber (inputNumber);
-    addToVariable (inputNumber);
+    /*addToVariable (inputNumber);*/
 
 }
 
@@ -37,19 +81,29 @@ function displayNumber(inputNumber) {
 display.value += inputNumber;
 }
 
-function addToVariable (){};
+function addToVariable(inputNumber){
+    firstInput.push(inputNumber);
+    history1=parseInt(firstInput.join(''))
+    console.log(history1);
+};
+
+function clearAll() {
+    location.reload();
+    /*
+    firstInput.splice(0, firstInput.length);
+    secondInput.splice(0, secondInput.length);
+    display.value= "";*/
 
 
-       /*
-        let userInput = button.id
-        if (display.value === "0") {
-            display.value = "";
-        }
-display.value += userInput;
-displayValue = display.value;
-console.log(displayValue)
-*/
+}
 
+function deleteNumber() {
+    firstInput.pop();
+    display.value=display.value.slice(0, -1);
+    history1=parseInt(firstInput.join(''))
+    console.log(history1);
+}
+      
   
 function addNumbers(a,b) {
     return a + b
